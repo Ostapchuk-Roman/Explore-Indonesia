bc233f36c20b386e6ece5a7f04e7919a7bcdd6bf
Ostapchuk-Roman <roma.ostapchuk2016@mail.com>
2025-03-19 22:15:59 +0300

    Оптимізував кнопку, підправив стилі, почав працювати над модальним вікном

--- test.js
+++ test.js
@@ -1,22 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const containers = document.querySelectorAll('.readmore-container');
-  const toggleButton = document.querySelector('.readmore-toggle');
+  const modal = document.getElementById('contactModal');
+  const openBtn = document.querySelector('.open-modal');
+  const closeBtn = document.querySelector('.close-modal');
 
-  containers.forEach(container => {
-    toggleButton.addEventListener('click', event => {
-      event.preventDefault(); // Запобігаємо стандартному переходу по лінку
+  // Відкриття модального вікна
+  openBtn.addEventListener('click', function () {
+    modal.style.display = 'flex';
+  });
 
-      const isExpanded = container.classList.contains('expanded');
+  // Закриття по хрестику
+  closeBtn.addEventListener('click', function () {
+    modal.style.display = 'none';
+  });
 
-      if (isExpanded) {
-        container.classList.remove('expanded');
-        container.style.maxHeight = '200px'; // Згортаємо до max-height
-        toggleButton.textContent = 'Read More';
-      } else {
-        container.classList.add('expanded');
-        container.style.maxHeight = 'none'; // Розгортаємо до повної висоти
-        toggleButton.textContent = 'Close';
-      }
-    });
+  // Закриття при кліку поза формою
+  window.addEventListener('click', function (event) {
+    if (event.target === modal) {
+      modal.style.display = 'none';
+    }
   });
 });
